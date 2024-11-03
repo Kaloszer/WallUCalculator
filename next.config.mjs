72329c327259995618f125654c8758a08f4420d2
kaloszer <seb.wiszowaty@gmail.com>
2024-11-03 18:40:48 +0100

    Move to experimental?

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: '/WallUCalculator',
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "ui.shadcn.com"
-      }
-    ]
+  experimental: {
+    images: {
+      unoptimized: true,
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "ui.shadcn.com"
+        }
+      ]
+    }
   },
   output: 'export'
   // output: 'standalone' // for docker deployment
